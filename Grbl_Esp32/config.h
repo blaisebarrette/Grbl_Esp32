049f14b1157de507a43e20cf57b5f23b1057a0c9
blaisebarrette <53702710+blaisebarrette@users.noreply.github.com>
2020-06-14 17:25:24 -0400

    All works!

--- Grbl_Esp32/config.h
+++ Grbl_Esp32/config.h
@@ -62,13 +62,13 @@ Some features should not be changed. See notes below.
 // NOTE: Defaults are set for a traditional 3-axis CNC machine. Z-axis first to clear, followed by X & Y.
 // These homing cycle definitions precede the machine.h file so that the machine
 // definition can undefine them if necessary.
-#define HOMING_CYCLE_0 (1<<Z_AXIS)	// TYPICALLY REQUIRED: First move Z to clear workspace.
-#define HOMING_CYCLE_1 (1<<X_AXIS)
-#define HOMING_CYCLE_2 (1<<Y_AXIS)
+//#define HOMING_CYCLE_0 (1<<Z_AXIS)	// TYPICALLY REQUIRED: First move Z to clear workspace.
+//#define HOMING_CYCLE_1 (1<<X_AXIS)
+//#define HOMING_CYCLE_2 (1<<Y_AXIS)
 
 // NOTE: The following is for for homing X and Y at the same time
-// #define HOMING_CYCLE_0 (1<<Z_AXIS) // first home z by itself
-// #define HOMING_CYCLE_1 ((1<<X_AXIS)|(1<<Y_AXIS))  // Homes both X-Y in one cycle. NOT COMPATIBLE WITH COREXY!!!
+#define HOMING_CYCLE_0 (1<<Z_AXIS) // first home z by itself
+#define HOMING_CYCLE_1 ((1<<X_AXIS)|(1<<Y_AXIS))  // Homes both X-Y in one cycle. NOT COMPATIBLE WITH COREXY!!!
 
 // Inverts pin logic of the control command pins based on a mask. This essentially means you can use
 // normally-closed switches on the specified pins, rather than the default normally-open switches.
